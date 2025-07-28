83afb253f46b6ab5eec9fd707a7ac718e4174adf
emudassar <contacmudassar@gmail.com>
2025-07-28 09:29:52 +0500

    solve 4 problems of variables

--- problems/concat.js
+++
@@ -1,3 +0,0 @@
-let a = 10
-
-console.log("10" + a);
---
+++ problems/variables.js
@@ -0,0 +1,36 @@
+// Update Score
+
+let score = 50;
+score = score + 20;
+
+console.log(score);
+
+// Constant Declaration
+
+
+const maxSize = 100;
+// maxSize = 200
+console.log(maxSize);
+// throwing an error after re assigning
+
+
+// Swap Two Variables
+
+let a = 5
+let b = 10
+let temp = a
+a = b
+b = temp
+console.log(a, b);
+
+
+// Counter Increment
+
+let counter = 0   
+counter = counter + 1
+counter = counter + 1
+counter = counter + 1
+counter = counter + 1
+counter = counter + 1
+console.log(counter);
+
