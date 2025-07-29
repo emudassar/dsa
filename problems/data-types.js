9548fae77ae505e5aaf8455ab9da0a73f3e07575
emudassar <contacmudassar@gmail.com>
2025-07-29 10:24:52 +0500

    practice data types

---
+++ problems/data-types.js
@@ -0,0 +1,18 @@
+// Declare an array scores with values [85, 90, 95]. Print the second element and the array length.
+// Example Output: 90 3
+
+
+let scores = [85, 90, 95]
+
+console.log(scores[1], scores.length);
+
+
+// Declare an object student with properties name (set to "Bob") and grade (set to 90). Print both properties.
+// Example Output: "Bob 90"
+
+let student = {
+    name: "Bob",
+    grade: 90
+}
+
+console.log(student.name, student.grade);
